1510596c29ec0ef8a5517f3bd49949dc5d3caca4
student <student@M90110>
2019-08-08 15:55:02 +0900

    이경준 member mapper/dao 작성중

--- TravelWithMe/src/dao/IMemberDao.java
+++ TravelWithMe/src/dao/IMemberDao.java
@@ -1,5 +1,15 @@
 package dao;
 
+import java.util.HashMap;
+import java.util.List;
+
+import model.Notice;
+
 public interface IMemberDao {
 
+	public String selectPw(String mid);
+	public List<String> selectLikecodeById(String mid);
+	public HashMap<String, Object> selectMemPoint(String mid);
+	public List<Notice> selectNoticeById(String mid);
+	
 }
