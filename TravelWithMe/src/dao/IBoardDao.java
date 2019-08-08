f691d083d3a45eb9bb4ef64c4d1effcaa40f9bb3
qhdms0795 <qhdms0795@gmail.com>
2019-08-08 16:03:59 +0900

    배보은 board mapper/Idao 작성중

--- TravelWithMe/src/dao/IBoardDao.java
+++ TravelWithMe/src/dao/IBoardDao.java
@@ -1,5 +1,24 @@
 package dao;
 
+import java.util.HashMap;
+import java.util.List;
+
+import model.AccomBoard;
+import model.Comments;
+import model.GuideBoard;
+
 public interface IBoardDao {
 
+	public List<HashMap<String, Object>> selectBoardById(String mid);
+	
+	public List<AccomBoard> selectAccomListByLike(String likecode);
+	
+	public List<GuideBoard> selectGuideListByRcnt();
+	
+	public AccomBoard selectOneAccom(int bnum);
+	public HashMap<String, Object> selectPolicyByBnum(int bnum);
+	public List<Comments> selectCommentList(int bnum);
+	public List<HashMap<String, Object>> selectRegisterListByBnum(int bnum);
+	public int getPossibleNop(int bnum);
+	public void insertRegister(int bnum, String regId,int nop);
 }
